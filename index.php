a631f482b7470e71118cbcdf39145d99c83e5fd0
zakariakodesolutiongit <83698891+zakariakodesolutiongit@users.noreply.github.com>
2022-10-26 15:27:09 +0600

    sdf

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-attachment_id3_data_meta_box( $post:WP_Post );
+
+$data = array(
+    "key1" => "value1",
+    "key2" => "value2",
+    "key3" => "value3",
+    "key4" => "value4",
+    "key4" => "value4",
+    "key4" => "value4",
+);
