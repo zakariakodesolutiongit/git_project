08db105c8b79009c708a83ed479f78dd27c5a23c
zakariakodesolutiongit <83698891+zakariakodesolutiongit@users.noreply.github.com>
2022-10-26 10:02:53 +0600

    sdf

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <h2>This is git test</h2>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis natus maiores sunt iure quis excepturi dolorum cum laudantium nam magnam quae dolore vitae, accusamus obcaecati modi. Reprehenderit tempora mollitia vel?</p>
-</body>
-</html>
+<?php
+
+<h2>This is php file</h2>
